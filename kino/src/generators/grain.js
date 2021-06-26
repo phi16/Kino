5b038ff5964064900bbcab6893fa46ad2f495f3a
phi16 <phi16.256@gmail.com>
2021-06-27 00:55:13 +0900

    Use forced attenuation / frequency adjustment

--- kino/src/generators/grain.js
+++ kino/src/generators/grain.js
@@ -4,12 +4,14 @@ module.exports = (Kino,o)=>{
   const I = Kino.I.sensel;
   let M = null;
 
+  const rawFreq = 656;
+  const storeThreshold = 10000; // 0.075;
   const notes = {};
   function retainNote(p, f) {
     if(notes[p]) return notes[p].acquire();
     const s = o.alloc();
     if(s == null) return null;
-    s.param([f/440, 0]);
+    s.param([f/rawFreq, 0]);
     let count = 0;
     let vel0 = 0, vel1 = 0, vel2 = 0, velM = 0;
     let volL = 0, volH = 0;
@@ -43,7 +45,7 @@ module.exports = (Kino,o)=>{
         velM = Math.max(velM, vel2);
         volH *= Math.exp(-dt*8);
         if(count == 0) {
-          if(volL < 0.075) volL *= Math.exp(-dt*2);
+          if(volL < storeThreshold) volL *= Math.exp(-dt*1);
           volume = volL + volH;
           vel1 = vel2;
         } else {
@@ -55,8 +57,8 @@ module.exports = (Kino,o)=>{
           volume = volL + volH;
           vel0 = 0;
         }
-        s.param([f/440, volume*volumeMult]);
-        if(count == 0 && volume < 0.001) {
+        s.param([f/rawFreq, volume*volumeMult]);
+        if(count == 0 && volume < 0.0001) {
           delete notes[p];
           s.release();
         }
@@ -151,7 +153,7 @@ module.exports = (Kino,o)=>{
     R.blend("lighter",_=>{
       R.translate(M.mainW/2,M.mainH/2).with(_=>{
         const vf = S.peakFreq;
-        vf.p = Math.log2(vf.f/440) * 12 - 12;
+        vf.p = Math.log2(vf.f/440) * 12 - 24;
         for(let i=-5;i<6;i++) {
           for(let j=-3;j<4;j++) {
             const x = i*1.5;
@@ -163,7 +165,7 @@ module.exports = (Kino,o)=>{
             const hue = x*0.02+y*0.05-0.2;
             const n = notes[p];
             const shape = n ? n.shape() : [0,0];
-            R.polyOutline(x*s,y*s,0.96*s*shapeDist(x,y),6,0,center?0.9:0.8).fill(hue,shape[0]>0.075?1:0,(center?0.03:0.01)*(1+touchBright[cp]*3));
+            R.polyOutline(x*s,y*s,0.96*s*shapeDist(x,y),6,0,center?0.9:0.8).fill(hue,shape[0]>storeThreshold?1:0,(center?0.03:0.01)*(1+touchBright[cp]*3));
             const vdi = Math.abs(vf.p-p);
             if(vdi < 1) {
               let str = 1 - vdi;
@@ -194,7 +196,7 @@ module.exports = (Kino,o)=>{
           t.m += ((t.d>0 ? 1 : 0.5) - t.m) * 0.4;
           R.polyOutline(x*s,y*s,0.96*s*shapeDist(x,y)*t.m,6,0,1-scale*0.3).fill(1,0,0.1*Math.pow(scale, 0.4));
         });
-        const centerFreq = 440*2/3*2;
+        const centerFreq = 440*2/3*4;
         const vCenter = (Math.log2(vf.f/centerFreq))*s*3;
         const vOffset = M.mainH*Math.sqrt(3)/6/2;
         R.line(vCenter-vOffset,-M.mainH/2,vCenter+vOffset,M.mainH/2).stroke(0,0,vf.s,1);
